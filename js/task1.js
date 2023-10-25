4601b888e3ab4e178b45aa054d03e743a4f1c7a6
diana <dskripka005@gmail.com>
2023-10-25 17:56:06 +0300

    fixfirstpoint

---
+++ js/task1.js
@@ -0,0 +1,17 @@
+//Завдання 1
+//Напиши функцію-конструктор Account, яка створює об'єкт з властивостями login і email. В prototype функції-конструктора добав метод getInfo(), який виводить в консоль значення полів login і email об'єкта який його викликав.
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
